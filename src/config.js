550c85494ec595ea293ae276a5d51e2d7b308e49
hbjs97 <hbjs97@naver.com>
2021-12-20 13:54:08 +0900

    etc, set production url

--- src/config.js
+++ src/config.js
@@ -1,10 +1,11 @@
 const config = {
-    // basename: only at build time to set, and Don't add '/' at end off BASENAME for breadcrumbs, also Don't put only '/' use blank('') instead,
-    // like '/berry-material-react/react/default'
-    basename: '',
-    defaultPath: '/dashboard',
-    fontFamily: `'Roboto', sans-serif`,
-    borderRadius: 12
+  // basename: only at build time to set, and Don't add '/' at end off BASENAME for breadcrumbs, also Don't put only '/' use blank('') instead,
+  // like '/berry-material-react/react/default'
+  basename: '',
+  defaultPath: '/dashboard',
+  fontFamily: `'Roboto', sans-serif`,
+  borderRadius: 12,
+  productionUrl: process.env.REACT_APP_PRODUCTION_URL || 'http://localhost:5100'
 };
 
 export default config;
